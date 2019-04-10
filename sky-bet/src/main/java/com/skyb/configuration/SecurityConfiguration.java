d37f2287bfcc573a45f2bb44c4ab82bbf5bfb484
smylar <smylar@ntlworld.com>
2019-04-10 16:45:35 +0100

    Change security config around so swagger-ui not blocked

--- sky-bet/src/main/java/com/skyb/configuration/SecurityConfiguration.java
+++ sky-bet/src/main/java/com/skyb/configuration/SecurityConfiguration.java
@@ -41,8 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-          .antMatchers("/public/**").permitAll()
-          .anyRequest().authenticated()
+          .antMatchers("/control/**").authenticated()
           .and()
           .httpBasic()
           .and()
